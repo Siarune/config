ab70502a55753c997f3afab5c25c91204d63c6af
siarune <aidan.sharp@siarune.dev>
2024-12-05 13:19:01 -0500

    organize, switch drives

--- sys/quetz/hardware.nix
+++ sys/quetz/hardware.nix
@@ -11,58 +11,68 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_xanmod_latest;
-  boot.initrd.availableKernelModules = [
-    "nvme"
-    "xhci_pci"
-    "ahci"
-    "usbhid"
-    "usb_storage"
-    "sd_mod"
-  ];
-  boot.initrd.kernelModules = [ ];
-  #   boot.kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    loader.systemd-boot.enable = true;
+    kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_xanmod_latest;
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+
+    initrd = {
+      kernelModules = [ ];
+      availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "ahci"
+        "usbhid"
+        "usb_storage"
+        "sd_mod"
+      ];
+    };
+
+  };
 
   # Ignores wakeup call from NVME device that keeps it from suspending
   services.udev.extraRules = ''
     ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1022" ATTR{device}=="0x1483" ATTR{power/wakeup}="disabled"
   '';
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/7f9ef1f3-96c6-4e26-848d-16b338bd9ded";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/24FE-6571";
-    fsType = "vfat";
-    options = [
-      "fmask=0022"
-      "dmask=0022"
-    ];
-  };
-
   swapDevices = [
     { device = "/dev/disk/by-uuid/e249f8fa-39b5-4466-a081-295f5429aede"; }
   ];
 
   fileSystems = {
+    ## DO NOT TOUCH ##
+    "/" = {
+      device = "/dev/disk/by-uuid/7f9ef1f3-96c6-4e26-848d-16b338bd9ded";
+      fsType = "ext4";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-uuid/24FE-6571";
+      fsType = "vfat";
+      options = [
+        "fmask=0022"
+        "dmask=0022"
+      ];
+    };
+
+    ## Nonessential, can touch
+
     "/mnt/m2" = {
       device = "/dev/disk/by-label/m2";
       fsType = "auto";
     };
+
     "/mnt/womp" = {
       device = "/dev/disk/by-partlabel/womp";
       fsType = "auto";
-      #       options = [];
     };
-    "/mnt/sat2" = {
-      device = "/dev/disk/by-partlabel/sat2";
+
+    "/mnt/exp" = {
+      device = "/dev/disk/by-label/exp";
       fsType = "auto";
     };
+
   };
 
   networking.hostName = "Quetz";
