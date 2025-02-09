4bc3038672873694dce23c8e8f4593d74e70dc81
siarune <aidan.sharp@siarune.dev>
2025-02-09 10:34:33 -0500

    package updates + zen browser

--- flake.nix
+++ flake.nix
@@ -23,6 +23,8 @@
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
 
   outputs =
--- usr/quetz/pkg.nix
+++ usr/quetz/pkg.nix
@@ -1,18 +1,40 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
 
   home.packages = with pkgs; [
     polychromatic
 
     freecad
+    logisim-evolution
 
     ryujinx
     heroic
     prismlauncher
     lutris
+    scarab
+    protonup-qt
     #     nexusmods-app-unfree
 
     obs-studio
+
+    qbittorrent
+    tor-browser
+
+    bottles
+
+    jetbrains.webstorm
+
+    qtcreator
+    kdePackages.kdevelop
+    kdePackages.full
+    gcc
+    gnumake
+    cmake
+    cmake-format
+    qbs
+    kdePackages.kirigami
+
+    inputs.zen-browser.packages."${system}".default
   ];
 
 }
