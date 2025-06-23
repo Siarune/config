067aa84ac89c33bc23f26c899d6f5e2d1817e9f3
siarune <asharp@siarune.dev>
2025-06-23 00:11:28 -0400

    direnv

--- usr/share/sh.nix
+++ usr/share/sh.nix
@@ -6,10 +6,63 @@
 {
   programs = {
 
-    bash = {
+    atuin = {
+      enable = true;
+      enableNushellIntegration = true;
+      enableBashIntegration = true;
+      flags = [
+        "--disable-up-arrow"
+      ];
+      settings = {
+        store_failed = false;
+      };
+    };
 
+    bash = {
       enable = true;
       enableCompletion = false;
+    };
+
+    carapace = {
+      enable = true;
+      enableNushellIntegration = true;
+      enableBashIntegration = true;
+    };
+
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      enableNushellIntegration = true;
+      nix-direnv.enable = true;
+    };
+
+    helix = {
+      enable = true;
+      defaultEditor = true;
+      settings = {
+        keys.normal = {
+          space.w = ":w";
+          space.q = ":q";
+        };
+        editor = {
+          line-number = "relative";
+          lsp.display-messages = true;
+        };
+      };
+
+      languages = {
+        language-server.nixd = with pkgs.nixd; {
+          command = "nixd";
+        };
+
+        language-server.typescript-language-server = with pkgs.nodePackages; {
+          command = "${typescript-language-server}/bin/typescript-language-server";
+          args = [
+            "--stdio"
+            "--tsserver-path=${typescript}/lib/node_modules/typescript/lib"
+          ];
+        };
+      };
     };
 
     nushell = {
@@ -76,88 +129,39 @@
           success_symbol = "[❯ ](green bold)";
           error_symbol = "[❯ ](red bold)";
         };
-
-        sudo = {
-          disabled = false;
-          format = "[!](sapphire bold)";
+        cmd_duration = {
+          format = "[$duration](yellow bold)";
         };
-
         directory = {
           home_symbol = "";
           read_only = " ";
         };
-
-        cmd_duration = {
-          format = "[$duration](yellow bold)";
-        };
-
-        format = lib.concatStrings [
-          "$directory"
-          "$git_branch"
-          "$git_status"
-          "$line_break"
-          "$shell"
-          "$nix_shell"
-          "$sudo"
-          "$character"
-        ];
-
-        right_format = lib.concatStrings [
-          "$cmd_duration"
-        ];
-
         shell = {
           disabled = false;
           bash_indicator = "#";
           nu_indicator = "";
         };
-      };
-    };
-
-    atuin = {
-      enable = true;
-      enableNushellIntegration = true;
-      enableBashIntegration = true;
-      flags = [
-        "--disable-up-arrow"
-      ];
-      settings = {
-        store_failed = false;
-      };
-    };
-
-    carapace = {
-      enable = true;
-      enableNushellIntegration = true;
-      enableBashIntegration = true;
-    };
-
-    helix = {
-      enable = true;
-      defaultEditor = true;
-      settings = {
-        keys.normal = {
-          space.w = ":w";
-          space.q = ":q";
-        };
-        editor = {
-          line-number = "relative";
-          lsp.display-messages = true;
-        };
-      };
-
-      languages = {
-        language-server.nixd = with pkgs.nixd; {
-          command = "nixd";
+        sudo = {
+          disabled = false;
+          format = "[!](sapphire bold)";
         };
 
-        language-server.typescript-language-server = with pkgs.nodePackages; {
-          command = "${typescript-language-server}/bin/typescript-language-server";
-          args = [
-            "--stdio"
-            "--tsserver-path=${typescript}/lib/node_modules/typescript/lib"
-          ];
-        };
+        format = lib.concatStrings [
+          # Line 1
+          "$directory"
+          "$git_branch"
+          "$git_status"
+          "$line_break"
+          # Line 2
+          "$shell"
+          "$nix_shell"
+          "$sudo"
+          "$character"
+        ];
+        right_format = lib.concatStrings [
+          "$cmd_duration"
+        ];
+
       };
     };
 
